9e6b06bff4bae106e435d3e10695750ece1c998c
Prasanth Vaaheeswaran <pvaahee@ca.ibm.com>
2019-02-26 16:00:16 -0500

    feat: Add mongo setup

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,38 @@
-const message = "Hello, World!";
-const sayHello = () => console.log(message);
+import dotenv from 'dotenv';
+import mongodb from 'mongodb';
+dotenv.config();
+const MongoClient = mongodb.MongoClient;
+const DB_HOST = process.env.DB_HOST;
+const options = {
+  authSource: process.env.DB_AUTH_SOURCE,
+  auth: {
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+  },
+  readPreference: process.env.DB_READ_PREFERENCE,
+  connectTimeoutMS: parseInt(process.env.DB_CONNECTION_TIMEOUT_MS, 10),
+  loggerLevel: process.env.DB_LOGGER_LEVEL,
+  replicaSet: process.env.DB_REPLICASET,
+  useNewUrlParser: true,
+};
 
-sayHello();
+const connect = async (url, driver, options) => {
+  const client = new driver.MongoClient(url, options);
+  try {
+    const dbServer = await client.connect();
+    return dbServer.db(process.env.DB);
+  } catch (e) {
+    throw e;
+  }
+};
+
+connect(
+  DB_HOST,
+  mongodb,
+  options,
+)
+  .then(db => db.listCollections().toArray())
+  .then(collections => {
+    collections.forEach(c => console.log(c.name));
+  })
+  .catch(err => console.log(err));
